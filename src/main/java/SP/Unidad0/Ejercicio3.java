44caa73bc2016aa53233a70457eac7b5d35ac8b4
DaniPraivet <danipraivet@gmail.com>
2025-09-23 16:08:33 +0200

    chore: misunderstood the problem

--- src/main/java/SP/Unidad0/Ejercicio3.java
+++ src/main/java/SP/Unidad0/Ejercicio3.java
@@ -4,8 +4,8 @@ import java.io.*;
 
 public class Ejercicio3 {
     /*
-    * Leer 2 ficheros de texto y se unirá en un nuevo fichero
-    */
+     * Leer 2 ficheros de texto y se unirá en un nuevo fichero
+     */
     public static void main(String[] args) {
         String filePath1 = "src/main/java/SP/Unidad0/Archivos/archivo1.txt";
         String filePath2 = "src/main/java/SP/Unidad0/Archivos/archivo2.txt";
@@ -14,21 +14,19 @@ public class Ejercicio3 {
         StringBuilder textoFusionado = new StringBuilder();
 
         try (BufferedReader br1 = new BufferedReader(new FileReader(filePath1));
-        BufferedReader br2 = new BufferedReader(new FileReader(filePath2));
+             BufferedReader br2 = new BufferedReader(new FileReader(filePath2));
              BufferedWriter bw = new BufferedWriter(new FileWriter(filePath3))) {
-            while ((texto = br1.readLine()) != null) {
-                textoFusionado.append(texto);
-                textoFusionado.append("\n");
-            }
-            while ((texto = br2.readLine()) != null) {
-                textoFusionado.append(texto);
-                textoFusionado.append("\n");
+            while (br1.readLine() == null && br2.readLine() == null) {
+                if ((texto = br1.readLine()) != null) {
+                    textoFusionado.append(texto);
+                }
+                if ((texto = br2.readLine()) != null) {
+                    textoFusionado.append(texto);
+                }
             }
             bw.write(String.valueOf(textoFusionado));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
