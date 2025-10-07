5b5ca0e734855053d3969127b8a91486d389affb
DaniPraivet <danipraivet@gmail.com>
2025-10-07 15:46:38 +0200

    feat(SP): Remove warning

--- src/main/java/SP/Unidad1/Ejercicio2/AbrirNotepad.java
+++ src/main/java/SP/Unidad1/Ejercicio2/AbrirNotepad.java
@@ -1,11 +1,12 @@
 package SP.Unidad1.Ejercicio2;
 
 public class AbrirNotepad {
+    static Process proceso;
     public static void main(String[] args) {
         ProcessBuilder processBuilder = new ProcessBuilder("calc.exe");
         for (int i = 0; i < 200; i++) {
             try {
-                Process proceso = processBuilder.start();
+                proceso = processBuilder.start();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
