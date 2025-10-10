9b49e34522fe83dbd08d6a327109707407c519ee
DaniPraivet <danipraivet@gmail.com>
2025-10-10 12:49:45 +0200

    chore(SP): exercise finished

--- src/main/java/SP/Unidad1/Ejercicio5/Principal.java
+++ src/main/java/SP/Unidad1/Ejercicio5/Principal.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 
 public class Principal {
     public static void main(String[] args) {
+        generarDatos(10);
         int suma = getSuma();
         System.out.println("Suma: " + suma);
     }
@@ -15,14 +16,28 @@ public class Principal {
             String[] ficheros = {"informatica.txt", "gerencia.txt", "contabilidad.txt", "comercio.txt", "rrhh.txt"};
             String[] ruta = System.getProperty("java.class.path").split(";");
             for (int i = 0; i < 5; i++) {
-                Process pHijo1 = new ProcessBuilder("java", "-cp", ruta[0], "SP.Unidad1.Ejercicio5.ProgramaContabilidad", ficheros[i]).start();
-                BufferedReader br = new BufferedReader(new InputStreamReader(pHijo1.getInputStream()));
-                String linea = br.readLine();
-                suma += Integer.parseInt(linea);
+                Process pHijo = new ProcessBuilder("java", "-cp", ruta[0], "SP.Unidad1.Ejercicio5.ProgramaContabilidad", ficheros[i], "r").start();
+                BufferedReader br = new BufferedReader(new InputStreamReader(pHijo.getInputStream()));
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    suma += Integer.parseInt(linea);
+                }
             }
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
         return suma;
+    }
+
+    public static void generarDatos(int numerosAGenerar) {
+        try {
+            String[] ficheros = {"informatica.txt", "gerencia.txt", "contabilidad.txt", "comercio.txt", "rrhh.txt"};
+            String[] ruta = System.getProperty("java.class.path").split(";");
+            for (int i = 0; i < 5; i++) {
+                Process pHijo = new ProcessBuilder("java", "-cp", ruta[0], "SP.Unidad1.Ejercicio5.ProgramaContabilidad", ficheros[i], "w", String.valueOf(numerosAGenerar)).start();
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
--- src/main/java/SP/Unidad1/Ejercicio5/ProgramaContabilidad.java
+++ src/main/java/SP/Unidad1/Ejercicio5/ProgramaContabilidad.java
@@ -1,7 +1,41 @@
 package SP.Unidad1.Ejercicio5;
 
+import java.io.*;
+
 public class ProgramaContabilidad {
     public static void main(String[] args) {
+        if (args[1].equals("r")) {
+            leerArchivo(args[0]);
+        } else {
+            escribirArchivo(args[0], Integer.parseInt(args[2]));
+        }
+    }
 
+    private static void leerArchivo(String archivo) {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/java/SP/Unidad1/Ejercicio5/"+archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encontro el archivo " + archivo);
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo " + archivo);
+        }
+    }
+
+    private static void escribirArchivo(String archivo, int numerosAGenerar) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("src/main/java/SP/Unidad1/Ejercicio5/"+archivo))) {
+            for (int i = 0; i < numerosAGenerar; i++) {
+                int numeroGenerado = (int) (Math.random()*100+1);
+                bw.write(String.valueOf(numeroGenerado));
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encontro el archivo " + archivo);
+        } catch (IOException e) {
+            System.out.println("Error al escribir el archivo " + archivo);
+        }
     }
 }
