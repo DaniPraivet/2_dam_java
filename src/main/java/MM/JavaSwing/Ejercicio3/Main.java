914d5c25ed6048dc2ad368499e6e256001a87af1
DaniPraivet <danipraivet@gmail.com>
2025-10-27 16:36:55 +0100

    feat(MM): Extra stuff

--- src/main/java/MM/JavaSwing/Ejercicio3/Main.java
+++ src/main/java/MM/JavaSwing/Ejercicio3/Main.java
@@ -1,6 +1,7 @@
 package MM.JavaSwing.Ejercicio3;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -32,15 +33,47 @@ public class Main extends JFrame {
                 if (e.getClickCount() == 2 && e.getButton() == MouseEvent.BUTTON1) {
                     System.out.println("Doble click");
                 } else {
-                    System.out.println("Botón presionado: " + e.getButton());
-                    System.out.println("Posición: " + e.getX() + "," + e.getY());
+                    System.out.println("Click");
                 }
+
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    mostrarMenuEmergente(e.getLocationOnScreen());
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println("Mouse pressed");
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                System.out.println("Mouse released");
+            }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                System.out.println("Mouse entered");
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                System.out.println("Mouse exited");
             }
         });
 
         setSize(800, 600);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void mostrarMenuEmergente(Point location) {
+        JMenuItem menu1 = new JMenuItem("Abrir");
+        JMenuItem menu2 = new JMenuItem("Nuevo");
+        JMenuItem menu3 = new JMenuItem("Salir");
+        JPopupMenu menu = new JPopupMenu("Menu");
+        menu.add(menu1);
+        menu.add(menu2);
+        menu.add(menu3);
+        menu.setLocation(location);
+        menu.setVisible(true);
     }
 
     public static void main(String[] args) {
